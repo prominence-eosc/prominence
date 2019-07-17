627ca6bcd8b1ffb96270255efb372d9955554b7b
Andrew Lahiff <andrew.lahiff@gmail.com>
2019-07-17 09:26:12 +0000

    Add ability to list files uploaded to cloud storage

--- frontend/backend.py
+++ frontend/backend.py
@@ -97,6 +97,37 @@ def redact_storage_creds(storage):
             storage['onedata']['token'] = '***'
     return storage
 
+def get_matching_s3_objects(url, access_key_id, secret_access_key, bucket, prefix="", suffix=""):
+    """
+    Generate objects in an S3 bucket filtered by a prefix and/or suffix
+    """
+    s3 = boto3.client('s3',
+                      endpoint_url=url,
+                      aws_access_key_id=access_key_id,
+                      aws_secret_access_key=secret_access_key)
+    paginator = s3.get_paginator("list_objects_v2")
+
+    kwargs = {'Bucket': bucket}
+
+    if isinstance(prefix, str):
+        prefixes = (prefix, )
+    else:
+        prefixes = prefix
+
+    for key_prefix in prefixes:
+        kwargs["Prefix"] = key_prefix
+
+        for page in paginator.paginate(**kwargs):
+            try:
+                contents = page["Contents"]
+            except KeyError:
+                return
+
+            for obj in contents:
+                key = obj["Key"]
+                if key.endswith(suffix):
+                    yield obj
+
 class ProminenceBackend(object):
     """
     PROMINENCE backend class
@@ -142,6 +173,39 @@ class ProminenceBackend(object):
                 return None
 
         return response
+
+    def list_objects(self, user, groups, path=None):
+        """
+        List objects in S3 storage
+        """
+        if path is None:
+            prefix = 'uploads/%s' % user
+            prefix_to_remove = ['uploads', user]
+        else:
+            prefix = 'uploads/%s' % path
+            prefix_to_remove = ['uploads']
+
+        print('[list_objects] prefix=',prefix)
+
+        objects = []
+         
+        try:
+            keys = get_matching_s3_objects(self._config['S3_URL'],
+                                           self._config['S3_ACCESS_KEY_ID'],
+                                           self._config['S3_SECRET_ACCESS_KEY'],
+                                           self._config['S3_BUCKET'],
+                                           prefix=prefix)
+        except Exception:
+            return objects
+
+        for key in keys:
+            name = key['Key']
+            pieces = name.split('/')
+            for item in prefix_to_remove:
+                pieces.remove(item)
+            objects.append('/'.join(pieces))
+
+        return objects
 
     def get_job_unique_id(self, job_id):
         """
@@ -1025,4 +1089,5 @@ class ProminenceBackend(object):
             with open(filename) as fd:
                 return fd.read()
         return None
+
 
