16fe71d499588e8fb250bf397aa442a9fc09da59
Andrew Lahiff <andrew.lahiff@gmail.com>
2019-12-16 07:45:42 +0000

    _output_urls was duplicated

--- frontend/prominence/backend/create_workflow.py
+++ frontend/prominence/backend/create_workflow.py
@@ -41,34 +41,6 @@ def output_params(workflow):
 
     return params
 
-def _output_urls(self, workflow, uid, label):
-    """
-    Generate output files/dirs
-    """
-    lists = ''
-    count = 0
-
-    for job in workflow['jobs']:
-        if 'outputFiles' in job:
-            for filename in job['outputFiles']:
-                url_put = self.create_presigned_url('put',
-                                                    self._config['S3_BUCKET'],
-                                                    'scratch/%s/%d/%s' % (uid, label, os.path.basename(filename)),
-                                                    604800)
-                lists = lists + ' prominenceout%d="%s" ' % (count, url_put)
-                count += 1
-
-        if 'outputDirs' in job:
-            for dirname in job['outputDirs']:
-                url_put = self.create_presigned_url('put',
-                                                    self._config['S3_BUCKET'],
-                                                    'scratch/%s/%d/%s.tgz' % (uid, label, os.path.basename(dirname)),
-                                                    604800)
-                lists = lists + ' prominenceout%d="%s" ' % (count, url_put)
-                count += 1
-
-    return lists
-
 def run(cmd, cwd, timeout_sec):
     """
     Run a subprocess, capturing stdout & stderr, with a timeout
