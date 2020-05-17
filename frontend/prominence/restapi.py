a70d04635ebf40b5557f6729436fae23cd426e87
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-05-17 08:23:17 +0000

    Tidying

--- frontend/prominence/restapi.py
+++ frontend/prominence/restapi.py
@@ -9,21 +9,19 @@ from data import data
 from jobs import jobs
 from workflows import workflows
 
-# Check required environment variable
-if 'PROMINENCE_RESTAPI_CONFIG_FILE' not in os.environ:
-    print('ERROR: Environment variable PROMINENCE_RESTAPI_CONFIG_FILE has not been defined')
-    exit(1)
-
-# Logging
-logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
-
-# Setup Flask
-app = Flask(__name__)
-app.config.from_pyfile(os.environ['PROMINENCE_RESTAPI_CONFIG_FILE'])
-app.register_blueprint(accounting)
-app.register_blueprint(data)
-app.register_blueprint(jobs)
-app.register_blueprint(workflows)
+logging.basicConfig(stream=sys.stdout,
+                    level=logging.INFO,
+                    format='%(asctime)s %(levelname)s %(message)s')
 
 if __name__ == "__main__":
+    if 'PROMINENCE_RESTAPI_CONFIG_FILE' not in os.environ:
+        logging.error('Environment variable PROMINENCE_RESTAPI_CONFIG_FILE has not been defined, exiting')
+        exit(1)
+
+    app = Flask(__name__)
+    app.config.from_pyfile(os.environ['PROMINENCE_RESTAPI_CONFIG_FILE'])
+    app.register_blueprint(accounting)
+    app.register_blueprint(data)
+    app.register_blueprint(jobs)
+    app.register_blueprint(workflows)
     app.run()
