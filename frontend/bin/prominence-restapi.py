daa0f2668d408254f180001273ae95d551fbc6c9
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-06-05 19:49:28 +0000

    Use pip to install

--- frontend/bin/prominence-restapi.py
+++ frontend/bin/prominence-restapi.py
@@ -4,10 +4,10 @@ import os
 import sys
 from flask import Flask
 
-from accounting import accounting
-from data import data
-from jobs import jobs
-from workflows import workflows
+from prominence import accounting
+from prominence import data
+from prominence import jobs
+from prominence import workflows
 
 logging.basicConfig(stream=sys.stdout,
                     level=logging.INFO,
@@ -15,10 +15,10 @@ logging.basicConfig(stream=sys.stdout,
 
 app = Flask(__name__)
 app.config.from_pyfile(os.environ['PROMINENCE_RESTAPI_CONFIG_FILE'])
-app.register_blueprint(accounting)
-app.register_blueprint(data)
-app.register_blueprint(jobs)
-app.register_blueprint(workflows)
+app.register_blueprint(accounting.accounting)
+app.register_blueprint(data.data)
+app.register_blueprint(jobs.jobs)
+app.register_blueprint(workflows.workflows)
 
 if __name__ == "__main__":
     if 'PROMINENCE_RESTAPI_CONFIG_FILE' not in os.environ:
---
+++ frontend/setup.py
@@ -0,0 +1,22 @@
+import re
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PROMINENCE",
+    version='1.0.0',
+    author="Andrew Lahiff",
+    author_email="andrew.lahiff@ukaea.uk",
+    description="PROMINENCE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://prominence-eosc.github.io/docs",
+    platforms=["any"],
+    install_requires=["uwsgi", "flask", "requests", "boto3", "PyJWT", "elasticsearch", "elasticsearch-dsl"],
+    package_dir={'': '.'},
+    scripts=["bin/prominence-restapi.py"],
+    packages=["prominence", "prominence.backend"],
+    package_data={"": ["README.md"]},
+)
