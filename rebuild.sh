a07e86fa1d5ecb0b45d77de06bf4f13e196774ec
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-08-28 12:16:42 +0000

    Add collectstatic

--- rebuild.sh
+++ rebuild.sh
@@ -4,4 +4,5 @@ rm db.sqlite3
 python3 manage.py makemigrations custom_user
 python3 manage.py makemigrations web
 python3 manage.py migrate
+python3 manage.py collectstatic
 python3 manage.py createsuperuser --username admin
