e4bce575fbe6ceebf8e00d1e7b85be2c8cada7af
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-08-29 06:33:28 +0000

    Migrate everything

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 find . -path '*/migrations/*.py*' -not -path './env/*' -not -name '__init__.py' -delete 
 rm db.sqlite3
-python3 manage.py makemigrations custom_user
-python3 manage.py makemigrations web
+python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic
 python3 manage.py createsuperuser --username admin
