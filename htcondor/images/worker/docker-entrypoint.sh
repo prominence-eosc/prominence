4731e5bb23ed075894ce50a193203343265ea9b6
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-09-03 14:11:12 +0000

    Modifications for token-based auth; tidying

--- htcondor/images/worker/docker-entrypoint.sh
+++ htcondor/images/worker/docker-entrypoint.sh
@@ -9,26 +9,20 @@ echo "PROM_NODES = $CONDOR_NODES" >> /etc/condor/config.d/docker
 echo "PROM_CORES_TOTAL = $CONDOR_CORES" >> /etc/condor/config.d/docker
 /usr/local/bin/get-location >> /etc/condor/config.d/docker
 
-# Complete setup of CA
-certhash=`openssl x509 -hash -noout -in /etc/condor/ca/root-ca.crt`
-ln -s /etc/condor/ca/root-ca.crt /etc/condor/ca/${certhash}.0
-ln -s /etc/condor/ca/root-ca.signing_policy /etc/condor/ca/${certhash}.signing_policy
+# Set ownership of token
+chown condor:condor /etc/condor/tokens.d/token.jwt
 
 # HTCondor execute directory
 mkdir -p /home/prominence
 chmod a+xrw /home/prominence
 mkdir -p /home/prominence/condor
 chown condor:condor /home/prominence/condor
-mkdir -p /home/user
-chown user /home/user
+mkdir -p /home/user/mounts
+chown -R user:user /home/user
+chown -R user:user /home/user/mounts
 
 if [ -d "$PROMINENCE_MOUNTPOINT" ]; then
     chown user $PROMINENCE_MOUNTPOINT
-fi
-
-if [ -d "/home/user/$PROMINENCE_MOUNTPOINT" ]; then
-    chown user /home/user/$PROMINENCE_MOUNTPOINT
-    chmod a+xrw /home/user/$PROMINENCE_MOUNTPOINT
 fi
 
 # Logs
