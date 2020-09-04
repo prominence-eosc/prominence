31c4e38ed39335826c66c00ebd32cff6bf595b23
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-09-04 12:37:52 +0000

    Include classes WorkflowStdOutView, WorkflowStdErrView

--- rest_api/views/__init__.py
+++ rest_api/views/__init__.py
@@ -1,3 +1,3 @@
 from .jobs import JobsView, JobStdOutView, JobStdErrView, JobRemoveFromQueue
-from .workflows import WorkflowsView
+from .workflows import WorkflowsView, WorkflowStdOutView, WorkflowStdErrView
 from .health import HealthView
