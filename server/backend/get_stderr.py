64b2f8e740dc656fac4d812c1d301d2be909330c
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-09-04 07:18:41 +0000

    Open files as read-only text

--- server/backend/get_stderr.py
+++ server/backend/get_stderr.py
@@ -6,13 +6,13 @@ def get_stderr(self, uid, iwd, out, err, job_id, job_name=None, instance_id=-1):
     """
     if instance_id > -1:
         if os.path.isfile('%s/job.%d.err' % (iwd, instance_id)):
-            with open('%s/job.%d.err' % (iwd, instance_id)) as fd:
+            with open('%s/job.%d.err' % (iwd, instance_id), 'rt') as fd:
                 return fd.read()
     elif os.path.isfile('%s/%s' % (iwd, err)):
-        with open('%s/%s' % (iwd, err)) as fd:
+        with open('%s/%s' % (iwd, err), 'rt') as fd:
             return fd.read()
     elif os.path.isfile('%s/%s/job.0.err' % (iwd, job_name)):
-        with open('%s/%s/job.0.err' % (iwd, job_name)) as fd:
+        with open('%s/%s/job.0.err' % (iwd, job_name), 'rt') as fd:
             return fd.read()
     elif os.path.isfile(err):
         with open(err) as fd:
--- server/backend/get_stdout.py
+++ server/backend/get_stdout.py
@@ -6,13 +6,13 @@ def get_stdout(self, uid, iwd, out, err, job_id, job_name=None, instance_id=-1):
     """
     if instance_id > -1:
         if os.path.isfile('%s/job.%d.out' % (iwd, instance_id)):
-            with open('%s/job.%d.out' % (iwd, instance_id)) as fd:
+            with open('%s/job.%d.out' % (iwd, instance_id), 'rt') as fd:
                 return fd.read()
     elif os.path.isfile('%s/%s' % (iwd, out)):
-        with open('%s/%s' % (iwd, out)) as fd:
+        with open('%s/%s' % (iwd, out), 'rt') as fd:
             return fd.read()
     elif os.path.isfile('%s/%s/job.0.out' % (iwd, job_name)):
-        with open('%s/%s/job.0.out' % (iwd, job_name)) as fd:
+        with open('%s/%s/job.0.out' % (iwd, job_name), 'rt') as fd:
             return fd.read()
     elif os.path.isfile(out):
         with open(out) as fd:
