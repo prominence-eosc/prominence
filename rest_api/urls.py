87a41571175dd018e1bfe8773771a1c6d39f1cbe
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-09-04 12:39:45 +0000

    Replaced string with str

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('v1/jobs/<int:job_id>/remove', views.JobRemoveFromQueue.as_view(), name='job_remove_from_queue'),
     path('v1/workflows', views.WorkflowsView.as_view(), name='workflows'),
     path('v1/workflows/<int:workflow_id>', views.WorkflowsView.as_view(), name='workflow'),
-    path('v1/workflows/<int:workflow_id>/<string:job>/stdout', views.WorkflowStdOutView.as_view(), name='workflow_get_stdout'),
-    path('v1/workflows/<int:workflow_id>/<string:job>/stderr', views.WorkflowStdErrView.as_view(), name='workflow_get_stderr'),
+    path('v1/workflows/<int:workflow_id>/<str:job>/stdout', views.WorkflowStdOutView.as_view(), name='workflow_get_stdout'),
+    path('v1/workflows/<int:workflow_id>/<str:job>/stderr', views.WorkflowStdErrView.as_view(), name='workflow_get_stderr'),
 ]
