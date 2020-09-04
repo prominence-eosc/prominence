a1113ba44063eb1c080e74464df394dcd61772a4
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-09-04 11:46:54 +0000

    Add route for dealing with specific workflows

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('v1/jobs/<int:job_id>/stderr', views.JobStdErrView.as_view(), name='job_get_stderr'),
     path('v1/jobs/<int:job_id>/remove', views.JobRemoveFromQueue.as_view(), name='job_remove_from_queue'),
     path('v1/workflows', views.WorkflowsView.as_view(), name='workflows'),
+    path('v1/workflows/<int:workflow_id>', views.WorkflowsView.as_view(), name='workflow'),
 ]
