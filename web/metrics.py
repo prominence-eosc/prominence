1ff126a71698ea5149999988fe8855879795288d
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-09-20 09:18:27 +0000

    Add job resource usage metrics

--- web/metrics.py
+++ web/metrics.py
@@ -28,3 +28,29 @@ class JobMetrics(object):
 
         return jobs_idle, jobs_running
 
+class JobResourceUsageMetrics(object):
+    def __init__(self, url, token, org, bucket):
+        self.client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+        self.bucket = bucket
+        self.org = org
+
+    def get_job(self, job_id, since):
+        query_api = self.client.query_api()
+
+        query = (' from(bucket:"' + self.bucket + '")'
+                 '|> range(start: -' + str(since) + 'm)'
+                 '|> filter(fn:(r) => r._measurement == "jobs_resource_usage")'
+                 '|> filter(fn:(r) => r.id == "' + str(job_id) + '")')
+
+        results = self.client.query_api().query(org=self.org, query=query)
+
+        memory = []
+
+        for table in results:
+            for row in table.records:
+                if row.values["_field"] == "memory":
+                    raw_time = row.values["_time"]
+                    new_time = raw_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+                    memory.append({'t': new_time, 'y': int(row.values["_value"])/1000})
+
+        return {'memory': memory}
