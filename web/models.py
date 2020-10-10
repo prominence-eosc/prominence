7fa78d898c2242953acadb838783037f453b6fb2
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-10-10 08:25:00 +0000

    Set OneData as default storage system

--- web/models.py
+++ web/models.py
@@ -16,7 +16,7 @@ class Storage(models.Model):
     password = models.CharField(max_length=250, blank=True)
     token = models.CharField(max_length=250, blank=True)
     space = models.CharField(max_length=120, blank=True)
-    storage_type = models.PositiveSmallIntegerField(choices=STORAGE_TYPES)
+    storage_type = models.PositiveSmallIntegerField(choices=STORAGE_TYPES, default=2, blank=False)
 
     def __str__(self):
         return self.name
