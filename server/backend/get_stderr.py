e2823f3f4061fd61115a62250f9fbd7cb0c21296
Andrew Lahiff <andrew.lahiff@gmail.com>
2020-12-15 22:31:23 +0000

    Return filename only if requested

--- server/backend/get_stderr.py
+++ server/backend/get_stderr.py
@@ -1,20 +1,28 @@
 import os
 
-def get_stderr(self, uid, iwd, out, err, job_id, job_name=None, instance_id=-1):
+def get_stderr(self, uid, iwd, out, err, job_id, job_name=None, instance_id=-1, content=True):
     """
     Return the stdout from the specified job
     """
     if instance_id > -1:
         if os.path.isfile('%s/job.%d.err' % (iwd, instance_id)):
+            if not content:
+                return '%s/job.%d.err' % (iwd, instance_id)
             with open('%s/job.%d.err' % (iwd, instance_id), 'rt') as fd:
                 return fd.read()
     elif os.path.isfile('%s/%s' % (iwd, err)):
+        if not content:
+            return '%s/%s' % (iwd, err)
         with open('%s/%s' % (iwd, err), 'rt') as fd:
             return fd.read()
     elif os.path.isfile('%s/%s/job.0.err' % (iwd, job_name)):
+        if not content:
+            return '%s/%s/job.0.err' % (iwd, job_name)
         with open('%s/%s/job.0.err' % (iwd, job_name), 'rt') as fd:
             return fd.read()
     elif err and os.path.isfile(err):
+        if not content:
+            return err
         with open(err) as fd:
             return fd.read()
     return None
--- server/backend/get_stdout.py
+++ server/backend/get_stdout.py
@@ -1,20 +1,28 @@
 import os
 
-def get_stdout(self, uid, iwd, out, err, job_id, job_name=None, instance_id=-1):
+def get_stdout(self, uid, iwd, out, err, job_id, job_name=None, instance_id=-1, content=True):
     """
     Return the stdout from the specified job
     """
     if instance_id > -1:
         if os.path.isfile('%s/job.%d.out' % (iwd, instance_id)):
+            if not content:
+                return '%s/job.%d.out' % (iwd, instance_id)
             with open('%s/job.%d.out' % (iwd, instance_id), 'rt') as fd:
                 return fd.read()
     elif os.path.isfile('%s/%s' % (iwd, out)):
+        if not content:
+            return '%s/%s' % (iwd, out)
         with open('%s/%s' % (iwd, out), 'rt') as fd:
             return fd.read()
     elif os.path.isfile('%s/%s/job.0.out' % (iwd, job_name)):
+        if not content:
+            return '%s/%s/job.0.out' % (iwd, job_name)
         with open('%s/%s/job.0.out' % (iwd, job_name), 'rt') as fd:
             return fd.read()
     elif out and os.path.isfile(out):
+        if not content:
+            return out
         with open(out) as fd:
             return fd.read()
     return None
