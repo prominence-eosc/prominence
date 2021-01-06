2c4ecacb7c97b52b5a10b6a17da97a3e8bb3a389
Andrew Lahiff <andrew.lahiff@gmail.com>
2021-01-06 11:27:49 +0000

    Need JobSnapshot class

--- rest_api/views/__init__.py
+++ rest_api/views/__init__.py
@@ -1,4 +1,5 @@
-from .jobs import JobsView, JobStdOutView, JobStdErrView, JobRemoveFromQueue
+from .jobs import JobsView, JobStdOutView, JobStdErrView, JobRemoveFromQueue, JobSnapshot
 from .workflows import WorkflowsView, WorkflowStdOutView, WorkflowStdErrView
 from .health import HealthView
 from .usage import UsageView
+from .data import DataView
