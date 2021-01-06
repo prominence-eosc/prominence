ceeda96a8e908153a735667e4cff778021701e00
Andrew Lahiff <andrew.lahiff@gmail.com>
2021-01-06 07:34:13 +0000

    Adding accounting (usage) API

--- rest_api/views/__init__.py
+++ rest_api/views/__init__.py
@@ -1,3 +1,4 @@
 from .jobs import JobsView, JobStdOutView, JobStdErrView, JobRemoveFromQueue
 from .workflows import WorkflowsView, WorkflowStdOutView, WorkflowStdErrView
 from .health import HealthView
+from .usage import UsageView
