85708397ffea87e7cb0008d0aeabf555baa5052f
Andrew Lahiff <andrew.lahiff@gmail.com>
2021-02-08 15:59:31 +0000

    Not needed when running process_completed_jobs.py using Supervisord

--- htcondor/hooks/process_completed_jobs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /home/prominence/git/prominence/env/bin/activate
-export PYTHONPATH=${PYTHONPATH}:/home/prominence/git/prominence
-python3 /usr/local/bin/process_completed_jobs.py
