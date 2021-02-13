cd061f3de8eb469e9d1801149c9f7f8001ce77b3
Andrew Lahiff <andrew.lahiff@gmail.com>
2021-02-13 07:13:00 +0000

    Add retries when submitting job

--- frontend/prominence/backend/create_job.py
+++ frontend/prominence/backend/create_job.py
@@ -1,8 +1,25 @@
 import os
 import shutil
+import time
 
 import classad
 import htcondor
+
+def submit_job(cjob):
+    data = {}
+    retval = 0
+
+    try:
+        sub = htcondor.Submit(cjob)
+        schedd = htcondor.Schedd()
+        with schedd.transaction() as txn:
+            cid = sub.queue(txn, 1)
+        data['id'] = cid
+    except Exception as err:
+        retval = 1
+        data = {"error":"Job submission failed with an exception"}
+
+    return (retval, data)
 
 def create_job(self, username, groups, email, uid, jjob):
     """
@@ -13,7 +30,6 @@ def create_job(self, username, groups, email, uid, jjob):
     if job_sandbox is None:
         return (1, {"error":"Unable to create job sandbox"})
 
-    # Copy executable to sandbox, change current working directory to the sandbox
     shutil.copyfile(self._promlet_file, os.path.join(job_sandbox, 'promlet.py'))
 
     os.chdir(job_sandbox)
@@ -26,19 +42,12 @@ def create_job(self, username, groups, email, uid, jjob):
     if status != 0:
         return (1, msg)
 
-    # Submit the job to HTCondor
-    data = {}
-    retval = 0
-
-    try:
-        sub = htcondor.Submit(cjob)
-        schedd = htcondor.Schedd()
-        with schedd.transaction() as txn:
-            cid = sub.queue(txn, 1)
-        data['id'] = cid
-    except Exception as err:
-        print('Exception submitting job to HTCondor:', err)
-        retval = 1
-        data = {"error":"Job submission failed with an exception"}
+    # Submit the job to HTCondor, including up to 5 retries
+    retval = 1
+    count = 0
+    while retval == 1 and count < 5:
+        (retval, data) = submit_job(cjob)
+        time.sleep(count*0.3)
+        count += 1
 
     return (retval, data)
