716c2e11f230ca813ff8260c21ddcb1f5c17868b
Andrew Lahiff <andrew.lahiff@gmail.com>
2021-02-24 16:13:50 +0000

    Display running & pending jobs current totals at top of page

--- frontend/views/basic.py
+++ frontend/views/basic.py
@@ -3,14 +3,27 @@ import logging
 
 from django.shortcuts import render
 
+from django.db.models import Q
+  
+from frontend.models import Job
+
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
+
+def get_jobs_overview(user):
+    jobs_pending = len(Job.objects.filter(Q(user=user) & (Q(status=0) | Q(status=1))))
+    jobs_running = len(Job.objects.filter(Q(user=user) & Q(status=2)))
+    return (jobs_pending, jobs_running)
 
 def index(request):
     if request.user.is_authenticated:
         xaxis_min = (datetime.now() - timedelta(hours=24)).strftime("%Y-%m-%dT%H:%M:%SZ")
         xaxis_max = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-        return render(request, 'home.html', {'xaxis_min': xaxis_min, 'xaxis_max': xaxis_max})
+        (jobs_pending, jobs_running) = get_jobs_overview(request.user)
+        return render(request, 'home.html', {'xaxis_min': xaxis_min,
+                                             'xaxis_max': xaxis_max,
+                                             'jobs_pending': jobs_pending,
+                                             'jobs_running': jobs_running})
     else:
         return render(request, 'index.html')
 
