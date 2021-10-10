2af4ef58b2b9ee0035e42f8ec0917e0bd8b9b83b
Andrew Lahiff <andrew.lahiff@gmail.com>
2021-10-10 07:33:15 +0000

    Handle binary content in stdout/err

--- frontend/prominence/backend/get_stdout.py
+++ frontend/prominence/backend/get_stdout.py
@@ -6,15 +6,15 @@ def get_stdout(self, uid, iwd, out, err, job_id, job_name=None, instance_id=-1):
     """
     if instance_id > -1:
         if os.path.isfile('%s/job.%d.out' % (iwd, instance_id)):
-            with open('%s/job.%d.out' % (iwd, instance_id)) as fd:
+            with open('%s/job.%d.out' % (iwd, instance_id), 'rb') as fd:
                 return fd.read()
     elif os.path.isfile('%s/%s' % (iwd, out)):
-        with open('%s/%s' % (iwd, out)) as fd:
+        with open('%s/%s' % (iwd, out), 'rb') as fd:
             return fd.read()
     elif os.path.isfile('%s/%s/job.0.out' % (iwd, job_name)):
-        with open('%s/%s/job.0.out' % (iwd, job_name)) as fd:
+        with open('%s/%s/job.0.out' % (iwd, job_name), 'rb') as fd:
             return fd.read()
     elif os.path.isfile(out):
-        with open(out) as fd:
+        with open(out, 'rb') as fd:
             return fd.read()
     return None
