2ace102d2e0a08b8e8e124f1267b74d86d991d7b
Andrew Lahiff <andrew.lahiff@gmail.com>
2021-12-09 07:55:49 +0000

    Check etcd; always check everything & return all errors

--- frontend/prominence/backend/health.py
+++ frontend/prominence/backend/health.py
@@ -1,33 +1,46 @@
 """Return status of HTCondor daemons and other services"""
-import requests
 import socket
+import requests
 import htcondor
 
 def get_health(self):
+    errors = []
+
     # Check collector
     try:
         coll = htcondor.Collector(socket.gethostname())
         if not coll.query(htcondor.AdTypes.Collector, 'true', ['Name']):
-            return (False, {'error': 'HTCONDOR_COLLECTOR'})
+            errors.append('HTCONDOR_COLLECTOR')
     except:
-        return (False, {'error': 'HTCONDOR_COLLECTOR'})
+        errors.append('HTCONDOR_COLLECTOR')
 
     # Check schedd
     try:
         coll = htcondor.Collector(socket.gethostname())
         schedds = coll.query(htcondor.AdTypes.Schedd, 'true', ['Name'])
         if not schedds:
-            return (False, {'error': 'HTCONDOR_SCHEDD'})
+            errors.append('HTCONDOR_SCHEDD')
     except:
-        return (False, {'error': 'HTCONDOR_SCHEDD'})
+        errors.append('HTCONDOR_SCHEDD')
 
     # Check Elasticsearch
     try:
         resp = requests.get('http://%s:%s' % (self._config['ELASTICSEARCH_HOST'], self._config['ELASTICSEARCH_PORT']))
         if resp.status_code != 200:
-            return (False, {'error': 'ELASTICSEARCH'})
+            errors.append('ELASTICSEARCH_STATUS')
     except:
-        return (False, {'error': 'ELASTICSEARCH'})
+        errors.append('ELASTICSEARCH_EXCP')
+
+    # Check etcd
+    try:
+        resp = requests.get('127.0.0.1:2379/health')
+        if resp.status_code != 200:
+            errors.append('ETCD_STATUS')
+    except:
+        errors.append('ETCD_EXCP')
+
+    if errors:
+        return (False, {'errors': ','.join(errors)})
 
     return (True, {})
 
