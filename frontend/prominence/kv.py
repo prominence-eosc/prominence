ce63e2f73aa8189af1cc04a6f7d67be45da8d8df
Andrew Lahiff <andrew.lahiff@gmail.com>
2021-12-22 10:03:19 +0000

    Support KV leases

--- frontend/prominence/kv.py
+++ frontend/prominence/kv.py
@@ -5,7 +5,7 @@ import etcd3
 from flask import Blueprint, jsonify, request
 from flask import current_app as app
 
-from .errors import func_disabled, kv_error, key_not_specified, no_value_provided, value_too_big
+from .errors import func_disabled, kv_error, key_not_specified, no_value_provided, value_too_big, ttl_not_specified, lease_not_found
 from .utilities import get_remote_addr
 
 from .auth import requires_auth
@@ -64,6 +64,47 @@ def get_keys(username, group, email, path=None):
 
     return base64.b64decode(value[0]).decode('utf-8')
 
+@kv.route("/prominence/v1/kv/lease", methods=['POST'])
+@requires_auth
+def create_lease(username, group, email):
+    """
+    Create a lease with the specified TTL
+    """
+    app.logger.info('%s CreateLease user:%s group:%s' % (get_remote_addr(request), username, group))
+
+    if app.config['ENABLE_KV'] != 'True':
+        return func_disabled()
+
+    ttl = None
+    lease_id = None
+    try:
+        if 'ttl' in request.get_json():
+            ttl = int(request.get_json()['ttl'])
+        if 'id' in request.get_json():
+            lease_id = request.get_json()['id']
+    except:
+        pass
+
+    if not ttl:
+        return ttl_not_specified()
+
+    lease = None
+    try:
+        etcd = etcd3.client()
+        if lease_id:
+            lease = etcd.lease(ttl, lease_id=lease_id)
+        else:
+            lease = etcd.lease(ttl)
+        etcd.close()
+    except Exception as err:
+        app.logger.error('Got exception creating leave: %s', err)
+        return kv_error()
+
+    if lease:
+        return jsonify({'id': lease.id}), 200
+
+    return kv_error()
+
 @kv.route("/prominence/v1/kv/<path:key>", methods=['POST'])
 @requires_auth
 def set_value(username, group, email, key=None):
@@ -84,12 +125,32 @@ def set_value(username, group, email, key=None):
     if sys.getsizeof(request.get_data()) > app.config['KV_MAX_BYTES']:
         return value_too_big()
 
+    value = None
+    lease_id = None
+    try:
+        if 'value' in request.get_json():
+            value = str(request.get_json()['value']).encode()
+        if 'lease_id' in request.get_json():
+            lease_id = request.get_json()['lease_id']
+    except:
+        pass
+
+    if not value:
+        value = request.get_data()
+
     try:
         etcd = etcd3.client()
-        etcd.put('/%s/%s' % (username, key), base64.b64encode(request.get_data()))
+
+        if not lease_id:
+            etcd.put('/%s/%s' % (username, key), base64.b64encode(value))
+        else:
+            etcd.put('/%s/%s' % (username, key), base64.b64encode(value), lease=lease_id)
         etcd.close()
     except Exception as err:
         app.logger.error('Got exception setting kv: %s', err)
+        if 'requested lease not found' in err:
+            return lease_not_found()
+
         return kv_error()
 
     return jsonify({}), 201
