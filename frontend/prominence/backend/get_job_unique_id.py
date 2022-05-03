9b30a71ea8995dc4e31a3ae16f2ffe8b9bc0b7ea
Andrew Lahiff <andrew.lahiff@gmail.com>
2022-05-03 07:24:23 +0000

    Bug fix

--- frontend/prominence/backend/get_job_unique_id.py
+++ frontend/prominence/backend/get_job_unique_id.py
@@ -42,7 +42,7 @@ def get_job_unique_id(self, job_id, return_qdate=False):
                 out = job['Out']
                 err = job['Err']
                 status = job['JobStatus']
-                date = job['QDate']
+                qdate = job['QDate']
                 # If a job has a DAGNodeName it must be part of a workflow, and to get the stdout/err of a such
                 # a job we need to know the job name
                 if 'DAGNodeName' in job:
