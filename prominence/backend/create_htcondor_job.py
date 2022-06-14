a7fbd3a6bdcbf5a79085cb3155dc6061dab8942c
Andrew Lahiff <andrew.lahiff@gmail.com>
2022-06-14 05:34:35 +0000

    Switch to number to make checking version easier

--- prominence/backend/create_htcondor_job.py
+++ prominence/backend/create_htcondor_job.py
@@ -102,7 +102,7 @@ def _create_htcondor_job(self, username, groups, email, uid, jjob, job_path, wor
         input_files.append(os.path.join(job_path, '.job.mapped.json'))
 
     # API version
-    cjob['+ProminenceAPI'] = condor_str('1.0')
+    cjob['+ProminenceAPI'] = 1.0
 
     # Standard defaults
     cjob['universe'] = 'vanilla'
