74cd5ae8dbb710474bfdf9c391f07fc772bc47f7
Andrew Lahiff <andrew.lahiff@gmail.com>
2022-06-20 15:04:09 +0000

    Sort by free CPUs descending

--- prominence/backend/resources.py
+++ prominence/backend/resources.py
@@ -22,6 +22,10 @@ def get_existing_resources(self, group):
             free = {'cpus': int(result["Cpus"]), 'memory': int(result["Memory"]/1024.0)}
             worker = {'capacity': capacity, 'free': free, 'site': result["ProminenceCloud"]}
             workers.append(worker)
+
+    # Sort by free CPUs descending
+    workers = sorted(workers, key=lambda x: x['free']['cpus'], reverse=True)
+
     data = {'existing': workers}
     
     return data
